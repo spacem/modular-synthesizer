85c39c15a86f4cfb463d299a77ca021520f9c96f
tekool <tekool@github.com>
2017-12-16 18:48:10 +0100

    Oscillator now uses a midi note as input.

--- src/app/main-panel/modules/instruments/oscillator/oscillator.component.ts
+++ src/app/main-panel/modules/instruments/oscillator/oscillator.component.ts
@@ -1,24 +1,42 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { WebMIDIService } from '../../../../shared/services/webmidi/webmidi.service';
 import { MainPanelService } from '../../../../shared/services/main-panel/main-panel.service';
 import { OscillatorService } from './oscillator.service';
+import { Subscription } from 'rxjs/Subscription';
+import { MidiNoteMessage } from '../../../../shared/models/midi/midi-note-message';
+import { ToneHelper } from '../../../../shared/helpers/tone-helper';
 
 @Component( {
 	selector: 'app-oscillator',
 	templateUrl: './oscillator.component.html',
 	styleUrls: [ './oscillator.component.scss' ]
 } )
-export class OscillatorComponent implements OnInit
+export class OscillatorComponent implements OnInit, OnDestroy
 {
 	@ViewChild('waveform') waveformSelect:ElementRef;
 	@ViewChild('tone') toneRange:ElementRef;
+
+	private noteSourceSubscription:Subscription;
+	private programSubscription:Subscription;
 
 	constructor( private mainPanelService:MainPanelService,  private webMIDIService:WebMIDIService, private oscillatorService:OscillatorService ){}
 
 	ngOnInit()
 	{
-		this.webMIDIService.keySource$.subscribe( key => this.setTone(key.on ? key.frequency : 0) );
-		this.webMIDIService.programSource$.subscribe( bank => this.setWaveformType(	['sine', 'square', 'sawtooth', 'triangle'][bank%4] as OscillatorType ) );
+		this.noteSourceSubscription = this.webMIDIService.noteSource$.subscribe( note => this.setNote(note) );
+		this.programSubscription = this.webMIDIService.programSource$.subscribe( program => this.setWaveformType(	['sine', 'square', 'sawtooth', 'triangle'][program%4] as OscillatorType ) );
+	}
+
+	ngOnDestroy()
+	{
+		this.noteSourceSubscription.unsubscribe();
+		this.programSubscription.unsubscribe();
+	}
+
+	public setNote( midiNoteMessage:MidiNoteMessage ):void
+	{
+		const tone = ToneHelper.noteToFrequency(midiNoteMessage.note);
+		this.oscillatorService.setTone(tone);
 	}
 
 	public setTone( tone:number ):void
