8a14354d224660bd63a31cd9a457f82e261e8fe6
tekool <tekool@github.com>
2017-12-16 16:29:03 +0100

    Added a MidiNoteMessage model.

---
+++ src/app/shared/models/midi/midi-note-message.ts
@@ -0,0 +1,25 @@
+export class MidiNoteMessage
+{
+	/**
+	 * Build a note MIDI message.
+	 *
+	 * @param {number} channel
+	 * 	The MIDI channel on which the «Note On/Off» message occurred.
+	 *
+	 * @param {number} midiNote
+	 * 	The note to take into account (with C-1 as reference zero note and G-9 as upper note, following MIDI standard).
+	 *
+	 * @param {number} velocity
+	 * 	The velocity of the note to take into account (from 0 to 127, following MIDI standard).
+	 *
+	 * @param {boolean} on
+	 * 	The note on or off state.
+	 */
+	constructor
+	(
+		readonly channel:number,
+		readonly midiNote:number,
+		readonly velocity:number,
+		readonly on:boolean
+	){}
+}
