a2ae4105f37d953b121229c1a256422a2cda74b6
tekool <github@tekool.net>
2017-12-19 17:30:24 +0100

    Theremin - Node start to play immediately.

--- src/app/shared/models/voice/voice.ts
+++ src/app/shared/models/voice/voice.ts
@@ -2,28 +2,32 @@ export class Voice
 {
 	protected oscillatorNode:OscillatorNode;
 	private outNode:AudioNode;
-	private tone:number;
-	private waveformType:OscillatorType;
+
+	/**
+	 * Build a voice instance.
+	 *
+	 * @param {number} oscillatorNumber
+	 * 	The number of allocatable oscillator.
+	 */
+	constructor( private oscillatorNumber:number=2 )
+	{
+
+	}
 
 	public setTone( tone:number ):void
 	{
 		if( this.oscillatorNode )
 		{
-			this.oscillatorNode.start( 0 );
 			this.oscillatorNode.frequency.setValueAtTime( tone, this.oscillatorNode.context.currentTime );
 		}
 		else
 			throw Error( `Trying to set oscillator tone on an oscillator that's not connected.` );
-
-		this.tone = tone;
 	}
 
 	public setWaveformType( waveformType:OscillatorType ):void
 	{
 		if( this.oscillatorNode )
 			this.oscillatorNode.type = waveformType;
-
-		this.waveformType = waveformType;
 	}
 
 	public connect( node:AudioNode )
@@ -46,10 +50,11 @@ export class Voice
 	{
 		if( outNode )
 		{
-			const oscillatorNode:OscillatorNode = outNode.context.createOscillator();
-			oscillatorNode.connect( outNode );
+			this.oscillatorNode = outNode.context.createOscillator();
+			this.oscillatorNode.connect( outNode );
+			this.oscillatorNode.frequency.setValueAtTime( 0, this.oscillatorNode.context.currentTime );
+			this.oscillatorNode.start( 0 );
 
-			this.oscillatorNode = oscillatorNode;
 		}
 		else
 			throw Error(`Trying to create an OscillatorNode without any output AudioNode.`);
