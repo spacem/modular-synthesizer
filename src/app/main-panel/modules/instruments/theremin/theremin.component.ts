f6bae83ca830f133dca84d12f2cf42169433624f
tekool <tekool@github.com>
2017-12-29 17:15:54 +0100

    Bounded theremin pad values (test implementation of logarithmic scale)

--- src/app/main-panel/modules/instruments/theremin/theremin.component.ts
+++ src/app/main-panel/modules/instruments/theremin/theremin.component.ts
@@ -4,6 +4,7 @@ import { MainPanelService } from '../../../../shared/services/main-panel/main-pa
 import { Subscription } from 'rxjs/Subscription';
 import { Voice } from '../../../../shared/models/voice/voice';
 
+
 @Component( {
 	selector: 'app-theremin',
 	templateUrl: './theremin.component.html',
@@ -12,7 +13,8 @@ import { Voice } from '../../../../shared/models/voice/voice';
 export class ThereminComponent implements OnInit, OnDestroy
 {
 	@ViewChild('waveform') waveformSelect:ElementRef;
-	@ViewChild('tone') toneRange:ElementRef;
+	@ViewChild('xRange') xRange:ElementRef;
+	@ViewChild('yRange') yRange:ElementRef;
 	@ViewChild('pad') pad:ElementRef;
 	@ViewChild('voices') voices:ElementRef;
 
@@ -20,7 +22,7 @@ export class ThereminComponent implements OnInit, OnDestroy
 	private programSubscription:Subscription;
 	private voice:Voice;
 
-	public maxFrequency:number = 4000;
+	public maxFrequency:number = 8000;
 
 	constructor( private mainPanelService:MainPanelService, private webMIDIService:WebMIDIService ){}
 
@@ -45,20 +47,13 @@ export class ThereminComponent implements OnInit, OnDestroy
 	// 		this.setTone(0);
 	// }
 
-	public start():void
-	{
+	public start():void{}
 
-	}
-
-	public stop():void
-	{
-
-	}
+	public stop():void{}
 
 	public setTone( tone:number ):void
 	{
 		this.voice.setTone(Number(tone));
-		this.toneRange.nativeElement.value = tone;
 	}
 
 	public setWaveformType( waveformType:OscillatorType ):void
@@ -75,7 +70,7 @@ export class ThereminComponent implements OnInit, OnDestroy
 		//TODO Make the real connection thing (probably don't need the main gain reference, just AudioContext or vice versa.
 		this.voice = Voice.createVoice(this.mainPanelService.getMainGain(),this.voices.nativeElement.value);
 		this.setWaveformType(this.waveformSelect.nativeElement.value);
-		this.setTone(this.toneRange.nativeElement.value);
+		this.setTone(this.xRange.nativeElement.value);
 	}
 
 	public disconnect():void
@@ -83,44 +78,111 @@ export class ThereminComponent implements OnInit, OnDestroy
 		this.voice.disconnect();
 	}
 
-	public onMouseMove($event:MouseEvent):void
+	/**
+	 * Handles mouse move events on "mouseable" devices.
+	 *
+	 * @param {MouseEvent} $event
+	 * 	Standard touch event emitted by the DOM.
+	 */
+	public onMouseMove( $event:MouseEvent ):void
+	{
+		if( $event )
+			this.onMove( $event.clientX, $event.clientY );
+	}
+
+	/**
+	 * Handles touch move events on "toucheable" devices.
+	 *
+	 * @param {TouchEvent} $event
+	 * 	Standard touch event emitted by the DOM.
+	 */
+	public onTouchMove( $event:TouchEvent ):void
 	{
 		if( $event )
 		{
-			const target:HTMLElement = $event.target as HTMLElement;
+			if( $event.touches && $event.touches[0] )
+			{
+				const touch:Touch = $event.touches[0];
+				this.onMove( touch.clientX, touch.clientY );
+			}
 
-			const x:number = Math.max(0, 		Math.min($event.clientX - target.offsetLeft,		 target.offsetWidth) );
-			const y:number = Math.max(0, 		Math.min($event.clientY - target.offsetTop,		 target.offsetHeight) );
-
-			this.onMove(x,y,target.offsetWidth,target.offsetHeight);
+			// Avoid unwanted page scroll when dragging screen of mobile devices.
+			$event.preventDefault();
 		}
-
-		$event.preventDefault();
 	}
 
-	public onTouchMove($event:TouchEvent):void
+	/**
+	 * Set the current pointer position on the pad HTML element
+	 *
+	 * @param {number} clientX
+	 * 	The x position of the pointer in the viewport.
+	 *
+	 * @param {number} clientY
+	 * 	The y position of the pointer in the viewport.
+	 */
+	private onMove(	clientX:number,	clientY:number ):void
 	{
-		if( $event.touches && $event.touches[0] )
-		{
-			const touch:Touch = $event.touches[0];
-			const target:HTMLElement = $event.target as HTMLElement;
+		const boundingClientRect:ClientRect = this.pad.nativeElement.getBoundingClientRect();
 
-			const x:number = Math.max(0, 		Math.min(touch.clientX - target.offsetLeft,		 target.offsetWidth) );
-			const y:number = Math.max(0, 		Math.min(touch.clientY - target.offsetTop,		 target.offsetHeight) );
+		// Values need to be bounded because mouse moves can be sent even outside the pad area.
+		const x:number = Math.max(0, 	Math.min(clientX - boundingClientRect.left, 		boundingClientRect.width 	) );
+		const y:number = Math.max(0, 	Math.min(clientY - boundingClientRect.top, 		boundingClientRect.height 	) );
 
-			this.onMove(x,y,target.offsetWidth,target.offsetHeight);
-		}
+		const percentX:number = x === 0 ? 0 : 100 / (boundingClientRect.width/x);
+		const percentY:number = y === 0 ? 0 : 100 / (boundingClientRect.height/y);
 
-		$event.preventDefault();
+		this.setX(percentX);
+		this.setY(percentY);
 	}
 
-	public onMove( x:number, y:number, width:number, height:number ):void
+	/**
+	 * Set the current X position of the pointer on the pad.
+	 *
+	 * @param {number} percent
+	 * 	The current X position of the pointer on the pad (expressed in percent)
+	 */
+	public setX( percent:number ):void
 	{
-		const maxFreq:number = this.maxFrequency;
-		const tone:number = maxFreq/(width/x);
+		//percent = this.percentLinearToLog(percent);
 
+		const tone:number = this.maxFrequency * percent/100;
 		this.setTone(tone);
-		//this.voice.setDetune(256/(height/y));
-		this.voice.setFilter(127/(height/y));
+
+		this.xRange.nativeElement.value = percent;
+
+		//console.log(Array(100).fill(0).map((e,i) => this.percentLinearToLog(i) ));
 	}
+
+	/**
+	 * Set the current Y position of the pointer on the pad.
+	 *
+	 * @param {number} percent
+	 * 	The current Y position of the pointer on the pad (expressed in percent)
+	 */
+	public setY( percent:number ):void
+	{
+		//const maxDetune:number = 256;
+		//const detune:number = maxDetune*(100/percent);
+		//this.voice.setFilter(detune);
+
+		const maxFilter:number = 127;
+		const filter:number = maxFilter * percent/100;
+		this.voice.setFilter(filter);
+
+		this.yRange.nativeElement.value = -percent;
+	}
+	//
+	// public percentLinearToLog( n:number )
+	// {
+	// 	if( n <= 0 )
+	// 		return 0;
+	//
+	// 	const minLog:number =  Math.log10(1);
+	// 	const maxLog:number = Math.log10(100);
+	// 	const range:number = maxLog - minLog;
+	//
+	// 	//return (Math.exp(n)-1)/(Math.E-1);
+	//
+	// 	return 100*(Math.log10(n) - minLog) / range;
+	// }
 }
